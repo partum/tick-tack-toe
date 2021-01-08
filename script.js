337d59d7e55e8b5939c556ec9ee1878a276b6698
partum <jarringt@uccs.edu>
2021-01-08 13:53:43 -0700

    adding functions
    add listeners to all boxes and switching between X and O

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var move = 'O'
 const cells = document.querySelectorAll('.cells')
+for (i=0; i<9; i++){
+    document.getElementById(i).addEventListener("click", displayMove);
+}
+function whoseTurn(){
+    if (move == 'O'){
+        move = 'X'
+    }
+    else if(move == 'X'){
+        move = 'O'
+    }  
+}
+function displayMove(){
+    whoseTurn();
+    var node = document.createTextNode(move);
+    document.getElementById(this.id).appendChild(node)
+}
